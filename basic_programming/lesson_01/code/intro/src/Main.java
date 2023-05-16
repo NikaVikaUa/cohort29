e8911b1dfec70d8dd8baf35b5296bda9b0a297e8
Evgenygroup <mailfrom61@gmail.com>
2023-05-16 21:17:58 +0200

    adds lesson 1

--- basic_programming/lesson_01/code/intro/src/Main.java
+++ basic_programming/lesson_01/code/intro/src/Main.java
@@ -1,2 +1,26 @@
 public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello Java");
+
+        System.out.print("My name");
+        System.out.print(" is");
+        System.out.println(" Evgeny");
+        System.out.println("Hello");
+    }
+    //Body  Тело класса
+    //    gtzhjj
+    //  переменная1
+    //  переменная2
+
+    // метод1
+    // метод2
+    /*
+    gkjvkjvbojbolbolvovojvobobobobbpib
+    jnjbjbkjbkjbkjbkjbkjbkbkbkb
+     */
+
+
+
 }
