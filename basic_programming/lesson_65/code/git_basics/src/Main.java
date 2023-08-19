a630605d42a3db8438bf4fa814be418af4bb5f74
Evgenygroup <mailfrom61@gmail.com>
2023-08-19 09:45:57 +0200

    adds lesson 65

---
+++ basic_programming/lesson_65/code/git_basics/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    /*
+    GIT
+
+    git repository - репозиторий
+    commit  коммит
+    pull - спулить
+    push - запушить
+
+    untracked - неотслеживаемый
+    modified - измененный
+    staged - подготовленный
+    commited - закомиченный
+     */
+}
