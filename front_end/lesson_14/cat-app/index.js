907ad380b66afa22fb904ca5da67d930879df7ca
Hypericum90 <142326174+Hypericum90@users.noreply.github.com>
2023-09-08 23:29:03 +0400

    Delete front_end/lesson_14 directory

--- front_end/lesson_14/cat-app/index.js
+++
@@ -1,8 +0,0 @@
-const catFactElement = document.getElementById("cat-facts");
-
-fetch("https://catfact.ninja/fact")
-.then((response)=> response.json())
-.then((obj)=> {
-    const { fact, length} = obj; // деструктуризация
-    catFactElement.textContent = `${fact} ${length}`
-})
