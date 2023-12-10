07946e16014c5d6533f79108ad7fa630f7135633
Sergey Iryupin <biblelamp@yandex.ru>
2023-12-10 07:33:27 +0100

    java backend lesson #25 init

---
+++ back_end/lesson_25/code/src/main/java/news/crawler/domain/SourceConfig.java
@@ -0,0 +1,33 @@
+package news.crawler.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class SourceConfig {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "source_config_id")
+    private Integer id;
+
+    private String rootUrl;
+    private String newsSuffix;
+    private String className;
+    private Boolean disabled;
+
+    public SourceConfig(String rootUrl, String newsSuffix) {
+        this.rootUrl = rootUrl;
+        this.newsSuffix = newsSuffix;
+    }
+}
