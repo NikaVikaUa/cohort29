5bb6d412d6db847df21e7df59325201bab7bbea9
Sergey Iryupin <biblelamp@yandex.ru>
2023-12-19 16:58:19 +0100

    java backend lesson #25 code update

--- back_end/lesson_25/code/src/main/java/news/crawler/controller/EventController.java
+++ back_end/lesson_25/code/src/main/java/news/crawler/controller/EventController.java
@@ -1,10 +1,10 @@
 package news.crawler.controller;
 
 import news.crawler.controller.dto.EventDTO;
-import news.crawler.controller.dto.EventShortDTO;
 import news.crawler.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,8 +17,20 @@ public class EventController {
     @Autowired
     private EventService eventService;
 
-    @GetMapping("/all")
-    public List<EventShortDTO> findAll() {
-        return eventService.findAll();
+    @GetMapping("/")
+    public List<EventDTO> findFirstPage() {
+        return eventService.findByPage(0, 10);
+    }
+
+    @GetMapping("/{page}/{pageSize}")
+    public List<EventDTO> findByPage(@PathVariable Integer page, @PathVariable Integer pageSize) {
+        return eventService.findByPage(page, pageSize);
+    }
+
+    @GetMapping("/check/{rootUrl}/{newsSuffix}/{className}")
+    public List<EventDTO> checkParser(@PathVariable String rootUrl,
+                                      @PathVariable String newsSuffix,
+                                      @PathVariable String className) throws Exception {
+        return eventService.checkParser(rootUrl, newsSuffix, className);
     }
 }
